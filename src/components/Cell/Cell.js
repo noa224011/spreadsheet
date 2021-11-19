d0cc7b6f6dd170983f6bacc393839af48a2f74ed
Noa Shay <noa224011224011@gmail.com>
2021-11-19 13:08:20 +0200

    calculate cell value on enter press

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,19 +1,18 @@
 import { useEffect, useRef, useState } from "react";
 import CellContext from "../../store/cell-context";
 import "./Cell.css";
+import { evaluate } from "mathjs";
 
 export const CELL_WIDTH = 100;
 export const CELL_HEIGHT = 25;
 
 function Cell(props) {
   const [isEditMode, setIsEditMode] = useState(false);
-  const [cellValue, setCellValue] = useState("cell");
+  const [cellValue, setCellValue] = useState("");
   const inputRef = useRef();
 
   function cellClickHandler() {
     console.log("EDIT MODE ON");
-    console.log(`cellId: ${props.cellId}`);
-    console.log("ref:", inputRef.current?.value);
     setIsEditMode(true);
   }
 
@@ -30,40 +29,76 @@ function Cell(props) {
 
   function updateCellValueHandler(event) {
     setCellValue(event.target.value);
+    console.log(`cellId: ${props.cellId}`);
+    console.log("ref:", inputRef.current?.value);
+    console.log("cellValue", cellValue);
   }
 
-  // useEffect(() => {
-  //   document.addEventListener("click", onClickOutsideInputHandler);
+  function calculate(value) {
+    if (value.startsWith("=")) {
+      const value = cellValue.slice(1);
+      try {
+        return evaluate(value);
+      } catch {
+        return value;
+      }
+    }
+  }
 
-  //   return () => {
-  //     document.addEventListener("click", onClickOutsideInputHandler);
-  //   };
-  // });
+  function handleClickOutsideCell(event) {
+    if (inputRef.current && !inputRef.current.contains(event.target)) {
+      console.log("input ref:", inputRef.current);
+      console.log("event target:", event.target);
+      console.log("You clicked outside of me!");
+      console.log("cell value:", cellValue);
 
-  const cellContext = {
-    cellValue,
-  };
+      // TODO: Add calculation here
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutsideCell);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutsideCell);
+    };
+  }, [inputRef]);
+
+  function onEnterClickCellHandler(event) {
+    if (event.key === "Enter") {
+      setIsEditMode(false);
+    }
+
+    if (cellValue) {
+      setTimeout(() => {
+        const outcome = calculate(cellValue);
+        setCellValue(outcome);
+      }, 1000);
+    }
+  }
+
+  // const cellContext = {
+  //   cellValue,
+  // };
 
   return isEditMode ? (
-    <CellContext.Provider value={cellContext}>
-      <input
-        className={"cell-input"}
-        ref={inputRef}
-        data-cell-id={props.cellId}
-        value={cellValue}
-        onChange={updateCellValueHandler}
-      ></input>
-    </CellContext.Provider>
+    // <CellContext.Provider value={cellContext}>
+    <input
+      className={isEditMode ? "cell-input cell-selected" : "cell-input"}
+      ref={inputRef}
+      data-cell-id={props.cellId}
+      value={cellValue}
+      onChange={updateCellValueHandler}
+      onKeyDown={onEnterClickCellHandler}
+    ></input>
   ) : (
-    <CellContext.Provider value={cellContext}>
-      <div
-        className={"cell-label"}
-        data-cell-id={props.cellId}
-        onClick={cellClickHandler}
-      >
-        {cellValue}
-      </div>
-    </CellContext.Provider>
+    // <CellContext.Provider value={cellContext}>
+    <div
+      className={"cell-label"}
+      data-cell-id={props.cellId}
+      onClick={cellClickHandler}
+    >
+      {cellValue}
+    </div>
   );
 }
 
