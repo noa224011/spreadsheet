21938efd674ad0e8f70acc15dd168d1ab97e4e4d
Noa Shay <noa224011224011@gmail.com>
2021-11-20 16:52:57 +0200

    letterToNumber helper function

---
+++ src/utils/lettersToNumbers.js
@@ -0,0 +1,6 @@
+export function letterToNumber(letter) {
+  //  prettier-ignore
+  const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  const upperLetter = letter.toUpperCase();
+  return alphabet.indexOf(upperLetter);
+}
